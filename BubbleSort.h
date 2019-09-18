bc281b9c124daadf44d2db332bb9e356672a6ae3
Anderle <DeAn@igg.local>
2019-09-18 14:21:16 +0200

    Bubble sort example

--- BubbleSort.h
+++ BubbleSort.h
@@ -1,20 +1,16 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
+#pragma once
+#include "iSort.h"
 
-class BubbleSort
+class BubbleSort : public iSort
 {
 public:
-	//Default constructor
+	//Default constructor calls the base constructor
 	BubbleSort()
-	{
-		Initialize();
-		Output();
-		getchar();
-	}
+		: iSort()
+	{}
 
 	//Where the magic happens
-	void Sort(int arr[], const int arrSize)
+	virtual void Sort(int arr[], const int arrSize) override
 	{
 		bool sorted = false;
 		while (!sorted)
@@ -31,47 +27,4 @@ public:
 			}
 		}
 	}
-
-	//Prints an array
-	void Print(int arr[], const int arrSize) const
-	{
-		std::cout << "-";
-		for (int i = 0; i < arrSize; i++)
-		{
-			std::cout << arr[i] << "-";
-		}
-		std::cout << std::endl;
-	}
-
-private:
-	//Initializes a vector with increasing numbers, 
-	//then shuffels the vector and then copies it to the array to be sorted
-	void Initialize()
-	{
-		nums.resize(ArrLength);
-		for (unsigned int i = 0; i < nums.size(); i++)
-		{
-			nums[i] = i + 1;
-		}
-		std::random_shuffle(nums.begin(), nums.end());
-		std::copy(nums.begin(), nums.end(), numArr);
-	}
-
-	//Prints the unsorted and sorted arrays
-	void Output()
-	{
-		std::cout << "NOT SORTED\n";
-		Print(numArr, ArrLength);
-		Sort(numArr, ArrLength);
-		std::cout << "\nSORTED\n";
-		Print(numArr, ArrLength);
-	}
-
-	//Defines the array length
-	static const int ArrLength = 50;
-	//Vector is only used to create the shuffeled array
-	std::vector<int> nums;
-	//the array to be sorted
-	int numArr[ArrLength];
-
 };

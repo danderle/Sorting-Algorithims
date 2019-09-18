a5be495a62f53ce5ee77c9b788ee77b9d835952e
Anderle <DeAn@igg.local>
2019-09-18 14:21:32 +0200

    Insertion sort example

--- InsertionSort.h
+++ InsertionSort.h
@@ -1,30 +1,16 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
+#pragma once
+#include "iSort.h"
 
-class InsertionSort
+class InsertionSort : public iSort
 {
 public:
+	//Default constructor calls the base constructor
 	InsertionSort()
-	{
-		nums.resize(ArrLength);
-		for (unsigned int i = 0; i < nums.size(); i++)
-		{
-			nums[i] = i + 1;
-		}
-		std::random_shuffle(nums.begin(), nums.end());
-		std::copy(nums.begin(), nums.end(), numArr);
+		: iSort()
+	{}
 
-		std::cout << "NOT SORTED\n";
-		Print(numArr, ArrLength);
-		Sort(numArr, ArrLength);
-		std::cout << "\nSORTED\n";
-		Print(numArr, ArrLength);
-
-		getchar();
-	}
-
-	void Sort(int arr[], const int arrSize)
+	//Where the magic happens
+	virtual void Sort(int arr[], const int arrSize) override
 	{
 		int key, j;
 		for (int i = 1; i < arrSize; i++)
@@ -40,20 +26,4 @@ public:
 			}
 		}
 	}
-
-	void Print(int arr[], const int arrSize) const
-	{
-		std::cout << "-";
-		for (int i = 0; i < arrSize; i++)
-		{
-			std::cout << arr[i] << "-";
-		}
-		std::cout << std::endl;
-	}
-
-private:
-	static const int ArrLength = 50;
-	std::vector<int> nums;
-	int numArr[ArrLength];
-
 };

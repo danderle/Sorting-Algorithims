34f8d038b34c54013c542604015aac7105d1784a
Anderle <DeAn@igg.local>
2019-09-25 17:04:23 +0200

    QuickSort finished

--- Algorithims/QuickSort.h
+++ Algorithims/QuickSort.h
@@ -11,26 +11,29 @@ public:
 	//Sorting algorithim to override
 	void Sort(int arr[], const int arrSize) override
 	{
+		ArrSize = arrSize;
 		DoQuickSort(arr, 0, arrSize - 1);
 	}
 
 private:
+	//Partitions the array according to most right number in array and then sorts to the left and to right
 	void DoQuickSort(int arr[], int low, int high)
 	{
 		if (low < high)
 		{
 			int partition = Partition(arr, low, high);
-
+			Print(arr, ArrSize);
 			DoQuickSort(arr, low, partition);
 			DoQuickSort(arr, partition + 1, high);
 		}
 	}
+	//compares the indexed number to the right with those to the left
 	int Partition(int arr[], int low, int high)
 	{
 		int i = low - 1;
 		int j = low;
 
-		for (; j < high-1; j++)
+		for (; j < high; j++)
 		{
 			if (arr[j] <= arr[high])
 			{
@@ -40,7 +43,7 @@ private:
 		}
 		i++;
 		std::swap(arr[i], arr[j]);
-		return i;
+		return --i;
 	}
-	
+	int ArrSize;
 };
